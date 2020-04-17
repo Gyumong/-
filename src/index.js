9078d8c8609ec50003e3c49283acf0b229aa9bf7
Gyumong <rlaalsrb3559@naver.com>
2020-04-17 18:38:55 +0900

    User,UserContainer 준비

--- src/index.js
+++ src/index.js
@@ -3,25 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
-import  rootReducer,{rootSaga} from './modules';
+import rootReducer, { rootSaga } from './modules';
 
-const sagaMiddleware= createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware();
 
-const store= createStore(rootReducer,window.__PRELOADED_STATE__,// 이값을 초기 상태로 사용함
-   applyMiddleware(thunk,sagaMiddleware));
+const store = createStore(
+  rootReducer,
+  window.__PRELOADED_STATE__, // 이 값을 초기상태로 사용함
+  applyMiddleware(thunk, sagaMiddleware)
+);
 
-   sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
